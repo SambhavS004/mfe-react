ee81384d78cf7e24416f05d981973ef25c6fb72b
SambhavS004 <sambhava31bbs@gmail.com>
2022-10-17 14:45:58 +0530

    Initial Commit

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin ({
+            name: 'ui_mfe_container',
+            remotes: {
+                marketing: 'ui-mfe-marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared: ['react', 'react-dom']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
---
+++ container/src/bootstrap.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const rootElem = document.querySelector('#ui-mfe-container-root');
+ReactDOM.render(<App />, rootElem);
