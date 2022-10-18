f51245a57a93ea641a4042b212231e82b54051de
SambhavS004 <sambhava31bbs@gmail.com>
2022-10-18 16:22:04 +0530

    updated code for distribution

--- container/src/App.js
+++ container/src/App.js
@@ -2,5 +2,11 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    return <MarketingApp />
+    return (
+        <div>
+            <h1>Marketing App from Container</h1>
+            <MarketingApp />
+        </div>
+        
+    )
 }
